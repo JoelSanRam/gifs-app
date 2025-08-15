e45e3e379f0235c461bc2ba7827ea6cf0e9e31ce
Joel <troya4888pumas@gmail.com>
2025-08-15 17:39:08 -0600

    feat: scroll infinito

--- src/app/gifs/pages/trending/trending.ts
+++ src/app/gifs/pages/trending/trending.ts
@@ -1,16 +1,28 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { GifList } from "../../components/gif-list/gif-list";
 import { Gifs } from '../../services/gifs';
 
 @Component({
   selector: 'app-trending',
-  imports: [GifList],
+  // imports: [GifList],
   templateUrl: './trending.html',
   styleUrl: './trending.css'
 })
 export default class Trending {
   gifService = inject(Gifs)
-  // gifs = signal(imageUrls)
+  scrollDivRf = viewChild<ElementRef>('groupDiv')
 
+  onScroll (event: Event){
+    const scrollDiv = this.scrollDivRf()?.nativeElement;
+    if(!scrollDiv) return;
+    const scrollTop = scrollDiv.scrollTop
+    const clientHeight = scrollDiv.clientHeight
+    const scrollHeight = scrollDiv.scrollHeight
+    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight
+    if(isAtBottom){
+      this.gifService.loadTrendingGifs()
+    }
+    console.log(isAtBottom)
+  }
 
 }
--- src/app/gifs/services/gifs.ts
+++ src/app/gifs/services/gifs.ts
@@ -18,6 +18,16 @@ const loadFromLocalStorage = () => {
 export class Gifs {
   private http = inject(HttpClient)
   trendingGifs = signal<Gif[]>([])
+  trendingGifsLoading = signal(false)
+  private trendingPage = signal(0)
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+    for (let i = 0; i < this.trendingGifs().length; i+=3) {
+      groups.push(this.trendingGifs().slice(i, i + 3))
+      
+    }
+    return groups
+  })
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage())
   searchHistorykeys = computed(() => Object.keys(this.searchHistory()))
 
@@ -31,14 +41,23 @@ export class Gifs {
   })
 
   loadTrendingGifs() {
+    if(this.trendingGifsLoading()) return;
+    this.trendingGifsLoading.set(true)
+    this.trendingPage.update((currentValue: number) => currentValue + 1)
+    const offset = this.trendingPage() * 20
     this.http.get<Giphy>(`${environment.giphyBaseUrl}/gifs/trending`,{
       params: {
         api_key: environment.giphyApiKey,
-        limit: 20
+        limit: 20,
+        offset: offset
       }
     }).subscribe((resp) => {
       const gifs = GifMapper.mapGiphyItemsToGifsArray(resp.data)
-      this.trendingGifs.set(gifs)
+      this.trendingGifs.update(currentGifs => [
+        ...currentGifs,
+        ...gifs
+      ])
+      this.trendingGifsLoading.set(false)
     })
   }
 
