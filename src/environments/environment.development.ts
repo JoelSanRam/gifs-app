b87e74852a648ebc54c41003934c75848ba677ba
Joel <troya4888pumas@gmail.com>
2025-08-15 18:16:01 -0600

    feat: typo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   companyName: 'Gifs',
   companyName2: 'App',
-  companySlogan: 'Gifs everyehere',
+  companySlogan: 'Gifs everywhere',
 
   gifsApp: 'hzAYubPpSs91sJYTKxl75lK4w9u2QbPt',
   giphyBaseUrl: 'https://api.giphy.com/v1',
